98b2aead6679a5ed2b1f200fd774f21a31391af0
duncanhawthorne <duncan.hawthorne@gmail.com>
2022-07-03 23:01:31 +0100

    Update

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -650,17 +650,23 @@ class _InfinitordleState extends State<Infinitordle> {
     return Stack(
       clipBehavior: Clip.none,
       children: [
-        AnimatedPositioned(
+        oneStepState == 0 ?
+        _sizedCard(index, boardNumber, val, bf)
+        : AnimatedPositioned(
           curve: Curves.fastOutSlowIn,
-          duration: Duration(milliseconds: oneStepState * durMult * 200),
+          duration: Duration(milliseconds: oneStepState * durMult * 200), //when oneStepState = 0 then will instantly transition
           top: -cardEffectiveMaxPixel * oneStepState,
-          child: SizedBox(
-            height: cardEffectiveMaxPixel,
-            width: cardEffectiveMaxPixel,
-            child: _card(index, boardNumber, val, bf),
-          ),
+          child: _sizedCard(index, boardNumber, val, bf),
         ),
       ],
+    );
+  }
+
+  Widget _sizedCard(index, boardNumber, val, bf) {
+    return SizedBox(
+      height: cardEffectiveMaxPixel,
+      width: cardEffectiveMaxPixel,
+      child: _card(index, boardNumber, val, bf),
     );
   }
 
